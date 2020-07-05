bf195d2f31d0f01f8653f62a193e0fc5f809ab23
yui0308ks <kazuyu03081@gmail.com>
2020-07-05 22:05:11 +0900

    initial commit

---
+++ spring.box/src/main/java/com/example/demo/config/WebMvcControllerAdvice.java
@@ -0,0 +1,16 @@
+package com.example.demo.config;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+public class WebMvcControllerAdvice {
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		dataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+	}
+
+}
